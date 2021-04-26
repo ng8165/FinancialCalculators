6515b8858b5a4cda91a84da0ce53325eebe9f0e0
Nelson Gou <nel4gou@gmail.com>
2021-04-26 11:26:54 -0700

    Update service worker registration

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // register service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/tax-yield-calc/service-worker.js').then(function(registration) {
+        navigator.serviceWorker.register('/FinancialCalculators/service-worker.js').then(function(registration) {
             console.log('[ServiceWorker] Registration Successful! Scope: ', registration.scope);
         }, function(err) {
             console.log('[ServiceWorker] Registration Failed... Error: ', err);
